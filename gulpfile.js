47c54c6b842bacf7c3d757b43b77037cfbb0eb39
serhiicss <serhii.css@gmail.com>
2016-12-05 20:58:15 +0300

    removed owl js error files

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('build', shell.task(['jekyll build --watch']));
 gulp.task('serve', function () {
     browserSync.init({server: {baseDir: '_site/'}});
 // Reloads page when some of the already built files changed:
-    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+    gulp.watch('_site/**/*.*', '_site/!mywork/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['build', 'serve']);
