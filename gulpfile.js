9c498db1e18bec9e1dbff710c81772675a0b094a
serhiicss <serhii.css@gmail.com>
2017-01-03 12:50:58 +0300

    -snowflakes, +new gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,35 @@
 var gulp = require('gulp');
-var shell = require('gulp-shell');
-var browserSync = require('browser-sync').create();
+		shell = require('gulp-shell');
+		sass           = require('gulp-sass'),
+		browserSync    = require('browser-sync'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		bourbon        = require('node-bourbon'),
+		notify         = require("gulp-notify");
+
+
+gulp.task('sass', function() {
+	return gulp.src('_sass/styles.sass')
+	.pipe(sass({
+		includePaths: bourbon.includePaths
+	}).on("error", notify.onError()))
+	//.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer(['last 15 versions']))
+	//.pipe(cleanCSS())
+	.pipe(gulp.dest('css'))
+	.pipe(browserSync.reload({stream: true}));
+});
 
 // Task for building blog when something changed:
-//gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
-
+// gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
 // Or if you don't use bundle:
 gulp.task('build', shell.task(['jekyll build --watch']));
 
 // Task for serving blog with Browsersync
 gulp.task('serve', function () {
     browserSync.init({server: {baseDir: '_site/'}});
-// Reloads page when some of the already built files changed:
+    // Reloads page when some of the already built files changed:
+    gulp.watch('_sass/**/*.sass', ['sass']);
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['build', 'serve']);
+gulp.task('default', ['sass', 'build', 'serve']);
