b2198e9f858cd6a11eca9cfbcbba477b7e93dbd3
serhiicss <serhii.css@gmail.com>
2018-01-06 21:21:50 +0300

    new website

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,112 @@
-// gulpfile setup bsed on:
-// https://aaronlasseigne.com/2016/02/03/using-gulp-with-jekyll/
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
 
-const child = require('child_process');
-const browserSync = require('browser-sync').create();
+// Set the banner content
+var banner = ['/*!\n',
+  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' */\n',
+  ''
+].join('');
 
-const gulp = require('gulp');
-const gutil = require('gulp-util');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const notify = require("gulp-notify");
-
-
-
-const siteRoot = '_site';
-const cssFiles = '_sass/**/*.sass';
-
-gulp.task('css', () => {
-	gulp.src(cssFiles)
-		.pipe(sass().on("error", notify.onError()))
-		.pipe(autoprefixer(['last 15 versions']))
-		.pipe(gulp.dest('css'));
+// Compiles SCSS files from /scss into /css
+gulp.task('sass', function() {
+  return gulp.src('scss/agency.scss')
+    .pipe(sass())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('jekyll', () => {
-	const jekyll = child.spawn('jekyll.bat', ['build',
-		'--watch',
-		'--incremental',
-		'--drafts'
-	]);
-
-	const jekyllLogger = (buffer) => {
-		buffer.toString()
-			.split(/\n/)
-			.forEach((message) => gutil.log('Jekyll: ' + message));
-	};
-
-	jekyll.stdout.on('data', jekyllLogger);
-	jekyll.stderr.on('data', jekyllLogger);
+// Minify compiled CSS
+gulp.task('minify-css', ['sass'], function() {
+  return gulp.src('css/agency.css')
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('serve', () => {
-	browserSync.init({
-		files: [siteRoot + '/**'],
-		port: 3000,
-		server: {
-			baseDir: siteRoot
-		}
-	});
-
-	gulp.watch(cssFiles, ['css']);
+// Minify custom JS
+gulp.task('minify-js', function() {
+  return gulp.src('js/agency.js')
+    .pipe(uglify())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('js'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('default', ['css', 'jekyll', 'serve']);
+// Copy vendor files from /node_modules into /vendor
+// NOTE: requires `npm install` before running!
+gulp.task('copy', function() {
+  gulp.src([
+      'node_modules/bootstrap/dist/**/*',
+      '!**/npm.js',
+      '!**/bootstrap-theme.*',
+      '!**/*.map'
+    ])
+    .pipe(gulp.dest('vendor/bootstrap'))
 
+  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest('vendor/jquery'))
 
-//---------------------------------------------
-// Push build to gh-pages                     |
-//---------------------------------------------
-gulp.task('deploy', function () {
-  return gulp.src("./site/**/*")
-    .pipe(deploy())
+  gulp.src(['node_modules/jquery.easing/*.js'])
+    .pipe(gulp.dest('vendor/jquery-easing'))
+
+  gulp.src([
+      'node_modules/font-awesome/**',
+      '!node_modules/font-awesome/**/*.map',
+      '!node_modules/font-awesome/.npmignore',
+      '!node_modules/font-awesome/*.txt',
+      '!node_modules/font-awesome/*.md',
+      '!node_modules/font-awesome/*.json'
+    ])
+    .pipe(gulp.dest('vendor/font-awesome'))
+})
+
+// Default task
+gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('css/*.css', ['minify-css']);
+  gulp.watch('js/*.js', ['minify-js']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('scss/*.scss', browserSync.reload);
+  gulp.watch('js/**/*.js', browserSync.reload);
 });
